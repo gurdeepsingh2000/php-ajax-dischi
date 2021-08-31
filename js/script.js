ecd89c3f51412e2f75d9079e6b6e3db6ab6bcaea
Gurdeep Singh <gurdeepsingh2000329@gmail.com>
2021-08-31 14:46:17 +0200

    update

--- js/script.js
+++ js/script.js
@@ -6,13 +6,25 @@ new Vue(
     el: '#app',
     data: {
     albums: [],
+    Genres: []
     },
-
-        created() {      
-            axios.get('http://localhost:8888/php-dischi/api/ApiDischi.php')
-                .then((result) => {
-                    this.albums = result.data;
-            })
+    created() {      
+        axios.get('http://localhost:8888/php-dischi/api/ApiDischi.php')
+            .then((result) => {
+                this.albums = result.data;
+        });
+        this.getGenre()
+    },
+    methods:{
+        getGenre(){
+            let arrayGen = []
+            for(let x = 0; this.albums.length > x; x++) {
+                if(!arrayGen.includes(this.albums[x].genre)){
+                    arrayGen.push(this.albums[x].genre);
+                }
+            }
+            this.Genres = arrayGen
         },
     }
+ }
 );
